42456e9a3ac893633c0cdc3cbed958d0024b8dbd
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-01-11 17:15:32 +0100

    feat: enhance CLI to accept dynamic resume file path as argument

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,16 @@ type Options = {
   headless?: boolean
 }
 
+program.version('1.0.0')
+program.description('A CLI for effortlessly rendering your JSON Resume')
+program.argument('<resume.json>', 'Path to resume JSON file')
 program.option('-d, --dir <dir>', 'Directory to save output files', 'result')
 program.option('-n, --name <name>', 'Name of the output files', 'resume')
 program.option('-w, --watch', 'Watch resume.json file for changes')
 program.option('--headless', 'Run browser in headless mode')
 program.parse()
 
+const [resumeFile] = program.args
 const { dir, name, watch, headless } = program.opts<Options>()
 
-render('resume.json', { dir, name, watch, headless })
+render(resumeFile!, { dir, name, watch, headless })
