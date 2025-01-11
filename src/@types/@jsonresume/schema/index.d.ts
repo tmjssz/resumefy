3277a50a3a682118fc60b359c3df77b93788098c
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-01-11 19:09:35 +0100

    refactor: simplify type definitions in @jsonresume/schema module

--- src/@types/@jsonresume/schema/index.d.ts
+++ src/@types/@jsonresume/schema/index.d.ts
@@ -1,12 +1,7 @@
 /// <reference types="node" />
 
 declare module '@jsonresume/schema' {
-  import { Validator, ValidationError } from 'jsonschema'
-  import * as schema from '@jsonresume/schema/schema.json'
-  import * as jobSchema from '@jsonresume/schema/job-schema.json'
+  import { ValidationError } from 'jsonschema'
 
   export function validate<T>(resumeJson: object, callback: (errors: ValidationError[], isValid: boolean) => T): T
-
-  export type scheme = typeof schema
-  export type jobSchema = typeof jobSchema
 }
