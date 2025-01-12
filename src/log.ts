6b6cdaede2c9d7aebe1e00459a5301d808b9c2cf
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-01-12 15:24:26 +0100

    feat: update logging methods to accept multiple arguments

--- src/log.ts
+++ src/log.ts
@@ -1,7 +1,7 @@
 import { blue, red, yellow } from 'ansicolor'
 
 export const log = {
-  warn: (message: unknown) => console.warn(yellow('[Warning]'), message),
-  error: (message: unknown) => console.error(red('[Error]'), message),
-  debug: (message: unknown) => console.error(blue('[Debug]'), message),
+  warn: (...args: unknown[]) => console.warn(yellow('[Warning]'), ...args),
+  error: (...args: unknown[]) => console.error(red('[Error]'), ...args),
+  debug: (...args: unknown[]) => console.error(blue('[Debug]'), ...args),
 }
