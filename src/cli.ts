c2aa39939144c66471d5059f6f19e3eecbbc7d94
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-01-12 01:12:28 +0100

    refactor: update CLI option description for clarity in argument naming

--- src/cli.ts
+++ src/cli.ts
@@ -12,7 +12,7 @@ export const cli = program
   .version('1.1.0')
   .description('A CLI for effortlessly rendering your JSON Resume')
   .argument('[resume.json]', 'path to resume JSON file', 'resume.json')
-  .option('-d, --outDir <dir>', 'directory to save output files', 'result')
+  .option('-d, --outDir <outDir>', 'directory to save output files', 'result')
   .option('-t, --theme <theme>', 'theme to use for rendering (overrides theme specified in resume.json)')
   .option('-w, --watch', 'watch resume.json file for changes')
   .option('--headless', 'run browser in headless mode')
