14689cd804998bf05151b7b2716567d4fe09c6d3
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-01-15 14:32:13 +0100

    refactor: improve parameter naming in init function and enhance validation error handling

--- src/init.ts
+++ src/init.ts
@@ -3,10 +3,10 @@ import { promises as fs } from 'fs'
 
 /**
  * Initialize a new resume JSON file with sample data and optional theme.
- * @param filename file name to write
- * @param theme name of theme to write in JSON file's meta data (optional)
+ * @param resumeFile File name to write
+ * @param theme Name of theme to write in JSON file's meta data (optional)
  */
-export const init = async (filename: string, theme?: string) => {
+export const init = async (resumeFile: string, theme?: string) => {
   const resume = theme ? { ...sampleResume, meta: { ...sampleResume.meta, theme } } : sampleResume
-  return fs.writeFile(filename, JSON.stringify(resume, null, 2))
+  return fs.writeFile(resumeFile, JSON.stringify(resume, null, 2))
 }
--- src/validate/index.ts
+++ src/validate/index.ts
@@ -4,10 +4,13 @@ import { validateObject } from './validate.js'
 /**
  * Validate a resume JSON file.
  * @param resumeFile The path to the resume JSON file
- * @returns true if resume is valid
- * @throws {ValidationError[]} Error if resume is invalid
+ * @returns Promise resolving with a boolean whether resume JSON is valid
  */
 export const validate = async (resumeFile: string) => {
   const resumeObject = JSON.parse(await fsPromises.readFile(resumeFile, 'utf-8'))
-  return validateObject(resumeObject)
+  try {
+    return validateObject(resumeObject)
+  } catch (err) {
+    return false
+  }
 }
