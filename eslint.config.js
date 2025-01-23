644538f68d62711a0cecc4cdc89410e06573570b
Tim Schwarz <4171783+tmjssz@users.noreply.github.com>
2025-01-23 11:38:55 +0100

    fix: update ESLint configuration to ignore unused variables with underscore prefix

--- eslint.config.js
+++ eslint.config.js
@@ -8,4 +8,17 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
 ]
